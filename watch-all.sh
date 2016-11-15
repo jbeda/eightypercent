d795b12f505d80d704f25bbe5d7c34462eee296b
Joe Beda <joe.github@bedafamily.com>
2016-11-15 12:22:42 -0800

    Regen bootstrap, increase font size

--- watch-all.sh
+++ watch-all.sh
@@ -8,7 +8,7 @@ set -o xtrace
 trap 'kill $(jobs -p)' EXIT
 
 # Recompile bootstrap as necessary
-fswatch-run bootstrap/variables.less ./customize-bootstrap.sh &
+fswatch bootstrap/variables.less | xargs -n1 ./customize-bootstrap.sh &
 
 # Have hugo serve and watch
 hugo -w -D -d test-public server &
